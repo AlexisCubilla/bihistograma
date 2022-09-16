392b1613116626c0fb9fb127c3a9dfc066db4771
AlexisCubilla <alexiscubilla500@gmail.com>
2022-09-16 02:37:43 -0400

    version semi terminada

--- prueba.m
+++ prueba.m
@@ -8,55 +8,50 @@ function rmain = main(img)
     end
 
     [count,label]=imhist(img);
+    
     h = [label, count];
-    imhist(img);
+    
+    % imhist(img);
 
     total = s(1)*s(2);
 
-
     pk_matriz = pk(h, total);
-    
     
     sp = SP(h, pk_matriz);
 
-    % plot(label,cantidad)
+    plot(h(:,1),h(:,2), "-" );title('histograma');
 
-    plot(h(:,1),h(:,2), "-" );
     xline(SP(h, pk_matriz),'red');
  
-
-    % [lmax,y] = size(h);
-    % lmax = lmax -1;
     lmin = h(1,1);
     lmax = h(end,1);
 
-    spl= splu(h(1:round(sp),:));
-    spu = splu(h(round(sp+1):end,:));
+    spl= splu(h(1:round(sp+1),:));
+    spu = splu(h(round(sp+2):end,:));
    
     [grl1, grl2, gru1, gru2] = GR(sp, spl, spu,lmin, lmax );
     
-    [pll1,pll2] = PL(h(1:round(sp),:), grl1, grl2);
-    [plu1,plu2] = PL(h(round(sp+1):end,:), gru1, gru2);
+    [pll1,pll2] = PL(h(1:round(sp+1),:), grl1, grl2);
+    [plu1,plu2] = PL(h(round(sp+2):end,:), gru1, gru2);
 
     
-
-    % yline(pll1)
-    % line([0 sp],[pll1 pll1],'color','blue')
-    % line([0 sp],[pll2 pll2],'color','blue')
-    % line([sp+1 lmax],[plu1 plu1],'color','blue')
-    % line([sp+1 lmax],[plu2 plu2],'color','blue')
-
-
+    line([0 sp],[pll1 pll1],'color','blue');
+    line([0 sp],[pll2 pll2],'color','blue');
+    line([sp lmax],[plu1 plu1],'color','blue');
+    line([sp lmax],[plu2 plu2],'color','blue');
 
     [hl,NL]=TRANSF(h(lmin+1:round(sp+1),:),pll1,pll2);
-
     [hu,NU]=TRANSF(h(round(sp+2):lmax+1,:),plu1,plu2);
 
-    h_trans=h;
+    h_tr=h;
 
-    hold on; plot(h_trans(:,1),h_trans(:,2), "-" );
+    h_tr(lmin+1:round(sp+1),:)=hl;
 
-    hold off;
+    h_tr(round(sp+2):lmax+1,:)=hu;
+    
+    
+    % hold on; plot(h_tr(:,1),h_tr(:,2),'-');
+
 
     pk_l = pk(hl,NL);
     pk_u = pk(hu,NU);
@@ -76,44 +71,9 @@ function rmain = main(img)
 
     ec(eclx+1:end,:)=ec_u;
 
-    img2=img_modif(img,ec);
+    rmain=img_modif(img,ec);
 
-    % figure;imshow(img2);
-
-    rmain=img2;
-
-
-    figure;imshow(img);title('original');figure;imshow(img2);title('modificado');
-
-    
-
-    % ec_l;
-
-    % ck_u
-
-    % hl
-    % hu
-    % % h_trans
-    % sp
-
-    % ec
-
-    % ec_l
-
-    % ec_u
-
-    % ec(ec_l(1,1)+1,ec_l(end,1)+1)=ec_l;
-    % ec(ec_u(1,1)+1,ec_u(end,1)+1)=ec_u;
-
-
-    
-    % img_modif=zeros(s(1),s(2));
-
-
-
-
-    % figure;imshow(ec_l);
-
+    figure;imshow(img);title('original');figure;imshow(rmain);title('modificado');
 
 
 end
